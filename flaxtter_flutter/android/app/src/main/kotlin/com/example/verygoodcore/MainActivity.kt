088d126cc648e846b1ba179ce061970f6be1358e
Teren91 <Teren_91@hotmail.com>
2024-03-19 13:31:19 +0100

    Change flutter app for very_good_cli flutter app

---
+++ flaxtter_flutter/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flaxtter_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flaxtter_flutter/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flaxtter_flutter/app/app.dart';
+import 'package:flaxtter_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
